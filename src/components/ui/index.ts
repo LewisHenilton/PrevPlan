bd2da7eb597fceddaaf6cee75019a2523c78b131
LewisHenilton <luizredacaok@gmail.com>
2025-12-16 21:21:46 -0300

    fix: remover export de Input.tsx vazio do index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,6 +3,3 @@ export { default as Button } from './Button';
 
   export { default as Card } from './Card';
   export type { CardProps, CardVariant } from './Card';
-
-  export { default as Input } from './Input';
-  export type { InputProps } from './Input';
